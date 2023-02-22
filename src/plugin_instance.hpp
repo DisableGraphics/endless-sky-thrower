5b175efdc180f073940a2121bddccb2c0e65387a
DisableGraphics <elchifladod@gmail.com>
2023-02-22 12:11:13 +0100

    ordererd

--- src/plugin_instance.hpp
+++ src/plugin_instance.hpp
@@ -12,27 +12,36 @@ class PluginInstance : public Gtk::VBox
 {
     public:
         PluginInstance(Plugin_ID id, bool is_installed);
+        
+        void download_plugin();
+        void uninstall_plugin();
+        
         //Getters
         Plugin_ID get_plugin_id();
         Gtk::Spinner * get_spinner();
         
         void set_installed(bool installed = true);
-        void download_plugin();
-        void uninstall_plugin();
+
+        
         
     private:
         Plugin_ID plugin_id;
+
         Gtk::Label name_label;
         Gtk::Label version_label;
         Gtk::Label author_label;
         Gtk::Label description_label;
         Gtk::Label license_label;
         Gtk::Label homepage_label;
+
         Gtk::Button install_button;
         Gtk::Button uninstall_button;
+
         Gtk::Separator separator;
-        Gtk::Spinner spinner;
         Gtk::Separator separator2;
+
+        Gtk::Spinner spinner;
+        
         void download();
         void uninstall();
 };
